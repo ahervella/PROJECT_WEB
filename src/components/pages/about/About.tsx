29be446fb6377d79c9c82937cb567c95556e8288
Alejandro Hervella <ahervella@me.com>
2026-09-01 18:54:44 +0200

    Adjusting about page
    now more accurately reflects skills on resumes

--- src/components/pages/about/About.tsx
+++ src/components/pages/about/About.tsx
@@ -13,20 +13,13 @@ import { AboutProgramLangEntry } from './AboutProgramLangEntry';
 
 import csharp from "$assets/skillsIcons/csharp.svg";
 import cpp from "$assets/skillsIcons/cpp.svg";
-import java from "$assets/skillsIcons/java.svg";
-import html from "$assets/skillsIcons/html5.svg"
-import css from "$assets/skillsIcons/css3.svg"
 import python from "$assets/skillsIcons/python.svg"
-import react from "$assets/skillsIcons/react.svg"
-import typescript from "$assets/skillsIcons/typescript.svg"
 import unity from "$assets/skillsIcons/unity.svg"
 import git from "$assets/skillsIcons/git.svg"
 import github from "$assets/skillsIcons/github.svg"
 import visualstudio from "$assets/skillsIcons/visualstudio.svg"
 import unreal from "$assets/skillsIcons/unreal.svg"
 import steam from "$assets/skillsIcons/steam.svg"
-import claude from "$assets/skillsIcons/claude.svg"
-import javascript from "$assets/skillsIcons/javascript.svg"
 import c from "$assets/skillsIcons/c.svg"
 import lua from "$assets/skillsIcons/lua.svg"
 import perforce from "$assets/skillsIcons/perforce.svg"
@@ -34,6 +27,10 @@ import wwise from "$assets/skillsIcons/wwise.svg"
 import latex from "$assets/skillsIcons/latex.svg"
 import pico8 from "$assets/skillsIcons/pico8.svg"
 import godot from "$assets/skillsIcons/godot.svg"
+import cmake from "$assets/skillsIcons/cmake.svg"
+import gdb from "$assets/skillsIcons/gdb.svg"
+import addresssanitizer from "$assets/skillsIcons/addresssanitizer.svg"
+import vim from "$assets/skillsIcons/vim.svg"
 
 function About(){
 
@@ -58,30 +55,34 @@ function About(){
                 <AboutProgramLangEntry imgPath={csharp} locKey="C#" />
                 <AboutProgramLangEntry imgPath={c} locKey="C" />
                 <AboutProgramLangEntry imgPath={lua} locKey="Lua" />
-                <AboutProgramLangEntry imgPath={typescript} locKey="TypeScript" />
-                <AboutProgramLangEntry imgPath={html} locKey="HTML" />
-                <AboutProgramLangEntry imgPath={css} locKey="CSS" />
-                <AboutProgramLangEntry imgPath={javascript} locKey="JavaScript" />
-                <AboutProgramLangEntry imgPath={java} locKey="Java" />
                 <AboutProgramLangEntry imgPath={python} locKey="Python" />
-
             </InfoPageSectionGrid>
+
+            <InfoPageSection subtitleLocKey='ABOUT_SYS_CONCEPTS' >
+                <InfoPageText className='textNorm IBM' locKey="ABOUT_SYS_CONCEPTS_BODY" />
+            </InfoPageSection>
 
             <InfoPageSectionGrid subtitleLocKey='ABOUT_TOOLS_TECH' centerItems={true}>
                 <AboutProgramLangEntry imgPath={unity} locKey="Unity" />
+                <AboutProgramLangEntry imgPath={wwise} locKey="Wwise" />
+                <AboutProgramLangEntry imgPath={perforce} locKey="Perforce" />
                 <AboutProgramLangEntry imgPath={git} locKey="Git" />
                 <AboutProgramLangEntry imgPath={github} locKey="GitHub" />
-                <AboutProgramLangEntry imgPath={perforce} locKey="Perforce" />
                 <AboutProgramLangEntry imgPath={visualstudio} locKey="Visual Studio" />
-                <AboutProgramLangEntry imgPath={react} locKey="React" />
-                <AboutProgramLangEntry imgPath={wwise} locKey="Wwise" />
-                <AboutProgramLangEntry imgPath={claude} locKey="Claude Code AI" />
-                <AboutProgramLangEntry imgPath={latex} locKey="LaTeX" />
+                <AboutProgramLangEntry imgPath={cmake} locKey="CMake" />
+                <AboutProgramLangEntry imgPath={gdb} locKey="GDB/LLDB" />
+                <AboutProgramLangEntry imgPath={addresssanitizer} locKey="AddressSanitizer" />
                 <AboutProgramLangEntry imgPath={unreal} locKey="Unreal Engine" />
                 <AboutProgramLangEntry imgPath={pico8} locKey="PICO-8" />
                 <AboutProgramLangEntry imgPath={godot} locKey="Godot Engine" />
                 <AboutProgramLangEntry imgPath={steam} locKey="Steamworks" />
+                <AboutProgramLangEntry imgPath={latex} locKey="LaTeX" />
+                <AboutProgramLangEntry imgPath={vim} locKey="Vim" />
             </InfoPageSectionGrid>
+
+            <InfoPageSection subtitleLocKey='ABOUT_FAMILIAR' >
+                <InfoPageText className='textNorm IBM' locKey="ABOUT_FAMILIAR_BODY" />
+            </InfoPageSection>
         </PageBase>
     
     )
